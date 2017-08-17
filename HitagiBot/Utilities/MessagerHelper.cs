380eb5ce0abcd6a9deb3efedb4dfd612998041b0
Walid <reusereducerecycle@botnets.me>
2017-08-17 03:59:35 -0400

    Implemented a message retry to handle 429 (Too Many Requests).

--- HitagiBot/Utilities/MessagerHelper.cs
+++ HitagiBot/Utilities/MessagerHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -15,10 +18,22 @@ namespace HitagiBot.Utilities
         public static async Task<Message> SendSmartTextMessageAsync(this TelegramBotClient botClient, Message source,
             string text, ParseMode parseMode = ParseMode.Default)
         {
+            Message resultMessage = null;
             var replyToMessageId = source.Chat.Type == ChatType.Private ? 0 : source.MessageId;
 
-            return await botClient.SendTextMessageAsync(source.Chat.Id, text, parseMode,
-                replyToMessageId: replyToMessageId);
+            for (var i = 0; i < 3; ++i)
+                try
+                {
+                    resultMessage = await botClient.SendTextMessageAsync(source.Chat.Id, text, parseMode,
+                        replyToMessageId: replyToMessageId);
+                    break;
+                }
+                catch (HttpRequestException)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1 * i));
+                }
+
+            return resultMessage;
         }
     }
 }
